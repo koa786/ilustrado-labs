2bf022c0829c4ab0d749c638f13040da168f3e8a
koa786 <koa786@gmail.com>
2026-07-16 07:04:04 +0500

    Add Microsoft Clarity tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,6 +28,8 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
             <Footer />
           </div>
         </ThemeProvider>
+        
+        {/* Google Analytics */}
           <Script
             src="https://www.googletagmanager.com/gtag/js?id=G-FTNLFLNKDN"
             strategy="afterInteractive"
@@ -41,7 +43,22 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
 
               gtag('config', 'G-FTNLFLNKDN');
             `}
-          </Script>     
+          </Script>
+
+        {/* Microsoft Clarity */}
+        <Script id="microsoft-clarity" strategy="afterInteractive">
+          {`
+            (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;
+                t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];
+                y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "xn4cfx2azd");
+          `}
+</Script>
+
+
  </body>
     </html>
   );
